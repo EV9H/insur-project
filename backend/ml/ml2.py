ff0e4808f1ba9307f6f2f06d3b2d52461cbc21a9
WZ <midas0231@gmail.com>
2023-12-22 02:51:03 -0500

    minor fix

--- backend/ml/ml2.py
+++ backend/ml/ml2.py
@@ -1,6 +1,5 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
 import pymysql
 import joblib
 
