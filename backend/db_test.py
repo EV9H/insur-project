c42605f764f88a6b5fea1442824d6d43c58a8e16
Haozhou(Evan) Huang <evanhuanghz@hotmail.com>
2023-12-22 01:14:31 -0500

    populate db

--- backend/db_test.py
+++ backend/db_test.py
@@ -1,5 +1,7 @@
 import pymysql
-import csv 
+import csv, os
+import random
+
 # Establish a connection to the database
 db = pymysql.connect(
     host='insurance-database.cdcuzzna0mo5.us-east-2.rds.amazonaws.com',
@@ -35,24 +37,191 @@ def createTableCustomer():
 
 # Execute a SQL query
 def InsertAll_Customer():
-    f = csv.reader(open('example.csv'))
+    f = csv.reader(open('C:/Users/EvanH/OneDrive/Documents/GitHub/insur-project/backend/util/csv/Customer.csv'))
     next(f)
-    q = """INSERT INTO customer(ssn,name, age, gender, income, health_rating, nChildren, married,purchased) 
-                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    q = """INSERT INTO Customer(fname, lname, mname, age, ssn, gender, active, suffix, income, health_rating, married,purchased) 
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
     for line in f:
         line = [None if cell == '' else cell for cell in line]
         cursor.execute(q, line)
        
-
+def InsertAll_Account():
+    f = csv.reader(open('C:/Users/EvanH/OneDrive/Documents/GitHub/insur-project/backend/util/csv/Account.csv'))
+    next(f)
+    q = """INSERT INTO Account(AccID, Password, Email, TaxID, GroupNum, AccCity, Active, AccCompany, AccName) 
+                        VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    for line in f:
+        line = [None if cell == '' else cell for cell in line]
+        cursor.execute(q, line)
+        
+def insert_Account():
+    q = """
+        INSERT INTO Account(AccID, Password, AccName) VALUES(test, 12345,testUser) 
+    
+    """
+    cursor.execute(q)
+    
+def connect_account_customer():
+    amount = 100
+    cursor.execute('''SELECT * FROM Customer''' )
+    customers = cursor.fetchall()
+    
+    cursor.execute('''SELECT * FROM Account''' )
+    accounts = cursor.fetchall()
+    
+    for i in range(amount):
+        ssn = customers[i][4]
+        accid = accounts[i][0]
+        custfname = customers[i][0]
+        custlname = customers[i][1]
+        
+        cursor.execute(""" INSERT INTO Account_owner(Ssn, AccID, CustFname, CustLname) VALUES(%s,%s, %s, %s)""", [ssn,accid,custfname,custlname])
+    
 # InsertAll_Customer()
-# cursor.execute(q)
-print("""FETCHING ------------- \n\n\n""")
-q = '''SELECT * FROM customer'''
+# InsertAll_Account()
+
+# connect_account_customer()
+def addBenefitType():
+    benefitType = "A"
+    benefitPercentage = "100%"
+    DurationOfPayment = '1y' 
+    
+    cursor.execute('''INSERT INTO BenefitType(BenefitType,BenefitPercentage,DurationOfPayment) VALUES(%s,%s,%s)''', [benefitType,benefitPercentage,DurationOfPayment] )
+
+    benefitType = "B"
+    benefitPercentage = "150%"
+    DurationOfPayment = '2y' 
+    
+    cursor.execute('''INSERT INTO BenefitType(BenefitType,BenefitPercentage,DurationOfPayment) VALUES(%s,%s,%s)''', [benefitType,benefitPercentage,DurationOfPayment] )
+
+    benefitType = "C"
+    benefitPercentage = "200%"
+    DurationOfPayment = '3y' 
+    
+    cursor.execute('''INSERT INTO BenefitType(BenefitType,BenefitPercentage,DurationOfPayment) VALUES(%s,%s,%s)''', [benefitType,benefitPercentage,DurationOfPayment] )
+
+def addPremiumType():
+    ptype = "A"
+    pinterval = "1m"
+    price = '100' 
+    
+    cursor.execute('''INSERT INTO PremiumType(PremiumType,PaymentInterval, Price) VALUES(%s,%s,%s)''', [ptype,pinterval,price] )
+
+    ptype = "B"
+    pinterval = "1m"
+    price = '150' 
+    
+    cursor.execute('''INSERT INTO PremiumType(PremiumType,PaymentInterval, Price) VALUES(%s,%s,%s)''', [ptype,pinterval,price] )
+
+    ptype = "C"
+    pinterval = "1m"
+    price = '300' 
+    
+    cursor.execute('''INSERT INTO PremiumType(PremiumType,PaymentInterval, Price) VALUES(%s,%s,%s)''', [ptype,pinterval,price] )
+
+def addPlan():
+    chars = ['A','B','C']
+    for ch in chars:
+        Plan_Name = "Plan "+ch
+        PremiumType = ch
+        BenefitType = ch
+        cursor.execute('''INSERT INTO Plan(Plan_Name, PremiumType,BenefitType) VALUES(%s,%s,%s)''', [Plan_Name,PremiumType,BenefitType])
+def add_region_manager_assoc_lic():
+    regionID = [1,2,3]
+    regions = ['New York', 'California', 'Shanghai']
+    for _ in range(3):
+        cursor.execute('''INSERT INTO Region(Region_Id, Region_Name) VALUES(%s,%s)''', [regionID[_], regions[_]])
+    cursor.execute(""" SELECT * FROM Region""")
+    
+    
+    license = [ i for i in range(0,10)]
+    for _ in range(len(license)):
+        cursor.execute("""INSERT INTO License(Lic_id) VALUES(%s)""", [license[_]])
+    
+    manager = ['987-65-4321']
+    cursor.execute("""INSERT INTO Manager(Manager_Ssn, Region_Id) VALUES(%s,%s)""", [manager[0],1])
+    print(cursor.fetchall())
+    assoc_ssn = [ str(i) * 3 + '-' + str(i)*2 + '-' + str(i)*4 for i in range(10) ]
+    for _ in range(len(assoc_ssn)):
+        cursor.execute('''INSERT INTO Associate(Assc_Ssn, Manager_Ssn, Lic_id, Region_Id) VALUES(%s,%s,%s,%s)''', [assoc_ssn[_], manager[0], license[_], regionID[_%3]])
+        
+    cursor.execute("""SELECT * FROM Associate""")
+    print(cursor.fetchall())
+def add_contract():
+    cursor.execute(""" SELECT Account.AccID, Age, Health_rating, Income, Married, Purchased FROM Account
+                        INNER JOIN Account_owner ao ON ao.AccID = Account.AccID
+                        INNER JOIN Customer c ON c.Ssn = ao.Ssn
+    """)
+    
+    
+    table = cursor.fetchall()
+    # print(table)
+    
+    accid  = [_[0] for _ in table]
+    age = [_[1] for _ in table]
+    hr = [_[2] for _ in table]
+    income = [_[3] for _ in table]
+    married = [_[4] for _ in table]
+    purchased = [_[5] for _ in table]
+    cnt = 0
+    for i in range(len(accid)):
+        _age = age[i] / 50 
+        _hr = 50 / hr[i] 
+        _income = 80000 / income[i]
+        _married = 0.8 if married[i] == '1' else 0.4
+        _purchased = 0.8 if purchased[i] == '1' else 0.4
+        
+        coefficient = _age * _hr * _income * _married * _purchased 
+        # print(_age, _hr, _income,_married, _purchased, "=", _age * _hr * _income * _married * _purchased)
+        
+        for j in range(random.randint(1,3)):
+            amount = random.randint(100,1000)
+            cid = cnt 
+            assoc = f'{str(i%10)*3}-{str(i%10)*2}-{str(i%10)*4}' 
+            planname = random.choice(['Plan A', 'Plan B', 'Plan C'])
+            acc = accid[i]
+            
+            status = '1' if coefficient * float(random.choice(['1','0.75', '1.25'])) > 0.5 else '0' 
+            cursor.execute("""INSERT INTO Contract(Amount, CID, AccID, Plan_Name, Assc_Ssn,Status) VALUES(%s,%s,%s,%s,%s,%s)""", [amount, cid, acc,planname,assoc,status])    
+            cnt += 1
+        
+        
+        
+    
+    
+
+
+    
+    
+# addPlan()
+# addBenefitType()
+# addPremiumType()
+# add_region_manager_assoc_lic()````
+# add_contract()
+# ------------------------
+# add_contract()
+# cursor.execute("""DELETE FROM Contract""")
+# cursor.execute("""SELECT CID, Status FROM Contract WHERE Status = 1 """)
+
+q = """
+    SELECT age, gender, income, health_rating, married, C.Status FROM Contract C
+    INNER JOIN Account_owner ON Account_owner.AccID = C.AccID
+    INNER JOIN Customer ON Customer.Ssn = Account_owner.Ssn 
+"""
 cursor.execute(q)
-results = cursor.fetchall()
-print(results)
+print(cursor.fetchall())
 
 
+# cursor.execute("SELECT * FROM Plan")
+# print(cursor.fetchall())
+# q = '''SELECT * FROM Account_owner ao
+
+#         INNER JOIN Account a ON ao.AccID = a.AccID
+#         INNER JOIN Customer c ON c.Ssn = ao.Ssn
+#     '''
+# cursor.execute(q)
+# results = cursor.fetchall()
+# print(results)
 db.commit()
 cursor.close()
 db.close()
--- backend/util/random_data.py
+++ backend/util/random_data.py
@@ -1,79 +1,186 @@
 import random 
-import csv
+import csv, os
 from faker import Faker
+# from db_query import QuerySender
 
-# ssn VARCHAR(255) PRIMARY KEY,
-#     name VARCHAR(255),
-#     age INT,
-#     gender VARCHAR(255),
-#     income FLOAT,
-#     health_rating INT,
-#     nChildren INT,
-#     married BOOLEAN
-
-# INSERT INTO customer (ssn, name, age, gender, income, health_rating, nChildren, married) VALUES ('123-45-6789', 'John Doe', 30, 'Male', 50000.0, 80, 2, True);
-ssn_counter = 111111111
-data = [['ssn', 'name', 'age', 'gender', 'income', 'health_rating', 'nChildren', 'married', 'purchased']]
-
+os.chdir('../backend/util/csv')
 faker = Faker()
 
-def generateRandomData(amount = 1000):
+# def generateRandomData(amount = 1000):
+#     data = [['ssn', 'name', 'age', 'gender', 'income', 'health_rating', 'nChildren', 'married', 'purchased']]
+#     ssn_counter = 111111111
+
+#     # wealthy
+#     wCnt = 0
+#     mCnt = 0
+#     pCnt = 0
+#     for i in range(0, int(amount * 0.2)): 
+#         pFactor = 1.2
+#         ssn = faker.ssn()
+#         name = faker.name()
+#         age = random.randint(20,100)
+#         gender = random.choice([0,1])
+#         income = round(random.uniform(80000, 200000), 2)
+#         health_rating = random.randint(1,100)
+#         nChildren = random.randint(0,3)
+#         married = random.choice([1,0])
+#         purchased = 1 if pFactor * (age/50) * (income/140000) * (80 / health_rating) > 1.5 else 0
+#         if purchased: 
+#             wCnt += 1
+#         data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+#     print("WEALTHY PURCHASED: ", wCnt, "probability = ", round( wCnt / (amount * 0.2),2) )
+#     # mid
+#     for i in range(0, int(amount * 0.4)): 
+#         pFactor = 0.8
+#         ssn = faker.ssn()
+#         name = faker.name()
+#         age = random.randint(20,100)
+#         gender = random.choice([0,1])
+#         income = round(random.uniform(40000, 80000), 2)
+#         health_rating = random.randint(1,100)
+#         nChildren = random.randint(0,3)
+#         married = random.choice([1,0])
+#         purchased = 1 if pFactor * (age/50) * (income/60000) * (80 / health_rating) > 1.5 else 0
+#         if purchased: 
+#             mCnt += 1
+#         data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+#     print("MID PURCHASED: ", mCnt, "probability = ", round( mCnt / (amount * 0.4),2) )
+    
+#     # poor
+#     for i in range(0, int(amount * 0.4)): 
+#         pFactor = 0.4
+#         ssn = faker.ssn()
+#         name = faker.name()
+#         age = random.randint(20,100)
+#         gender = random.choice([0,1])
+#         income = round(random.uniform(20000, 40000), 2)
+#         health_rating = random.randint(1,100)
+#         nChildren = random.randint(0,3)
+#         married = random.choice([1,0])
+#         purchased = 1 if pFactor * (age/50) * (income/30000) * (80 / health_rating) > 1.5 else 0
+#         if purchased: 
+#             pCnt += 1
+#         data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+#     print("poor PURCHASED: ", pCnt, "probability = ", round( pCnt / (amount * 0.4),2) )
+    
+
+
+    
+        
+
+def w(filename,data):
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
+        
+def fill_customer():
+    data = [['Fname', 'Lname', 'Mname', 'Age', 'Ssn', 'Gender', 'Active', 'Suffix', 'Income', 'Health_rating', 'Married', 'Purchased']]
+    
+    amount = 100
     
     # wealthy
     wCnt = 0
     mCnt = 0
     pCnt = 0
+    pFactor = 1.2
     for i in range(0, int(amount * 0.2)): 
-        pFactor = 1.2
-        ssn = faker.ssn()
-        name = faker.name()
+        name = faker.name().split(' ')
+        
+        fname = name[0]
+        lname = name[1]
+        mname = ''
         age = random.randint(20,100)
+        ssn = faker.ssn()
         gender = random.choice([0,1])
+        active = random.choice([0,1])
+        suffix = ''
         income = round(random.uniform(80000, 200000), 2)
+        
         health_rating = random.randint(1,100)
-        nChildren = random.randint(0,3)
         married = random.choice([1,0])
         purchased = 1 if pFactor * (age/50) * (income/140000) * (80 / health_rating) > 1.5 else 0
         if purchased: 
             wCnt += 1
-        data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+        data.append([fname, lname, mname, age, ssn, gender, active, suffix, income, health_rating, married,purchased])
     print("WEALTHY PURCHASED: ", wCnt, "probability = ", round( wCnt / (amount * 0.2),2) )
     # mid
     for i in range(0, int(amount * 0.4)): 
         pFactor = 0.8
-        ssn = faker.ssn()
-        name = faker.name()
+        name = faker.name().split(' ')
+        
+        fname = name[0]
+        lname = name[1]
+        mname = ''
         age = random.randint(20,100)
+        ssn = faker.ssn()
         gender = random.choice([0,1])
+        active = random.choice([0,1])
+        suffix = ''
         income = round(random.uniform(40000, 80000), 2)
         health_rating = random.randint(1,100)
-        nChildren = random.randint(0,3)
         married = random.choice([1,0])
         purchased = 1 if pFactor * (age/50) * (income/60000) * (80 / health_rating) > 1.5 else 0
         if purchased: 
             mCnt += 1
-        data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+        data.append([fname, lname, mname, age, ssn, gender, active, suffix, income, health_rating, married,purchased])
     print("MID PURCHASED: ", mCnt, "probability = ", round( mCnt / (amount * 0.4),2) )
     
     # poor
     for i in range(0, int(amount * 0.4)): 
         pFactor = 0.4
-        ssn = faker.ssn()
-        name = faker.name()
+        name = faker.name().split(' ')
+        
+        fname = name[0]
+        lname = name[1]
+        mname = ''
         age = random.randint(20,100)
+        ssn = faker.ssn()
         gender = random.choice([0,1])
+        active = random.choice([0,1])
+        suffix = ''
         income = round(random.uniform(20000, 40000), 2)
         health_rating = random.randint(1,100)
-        nChildren = random.randint(0,3)
         married = random.choice([1,0])
         purchased = 1 if pFactor * (age/50) * (income/30000) * (80 / health_rating) > 1.5 else 0
         if purchased: 
             pCnt += 1
-        data.append([ssn,name,age,gender,income,health_rating,nChildren,married,purchased])
+        data.append([fname, lname, mname, age, ssn, gender, active, suffix, income, health_rating, married,purchased])
     print("poor PURCHASED: ", pCnt, "probability = ", round( pCnt / (amount * 0.4),2) )
     
-generateRandomData()
 
-with open('example.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerows(data)
+    w("Customer.csv", data)
+    
+
+def fill_account():
+    faker2 = Faker()
+    data = [['AccID', 'Password', 'Email', 'TaxID', 'GroupNum', 'AccCity', 'Active', 'AccCompany', 'AccName']]
+    
+    amount = 100
+    
+    for _ in range(amount):
+        accid = str(faker2.unique.random_int(min=111111, max=999999))
+        password = str('12345')
+        email = faker2.email()
+        taxid = ''
+        groupnum = ''
+        acccity = faker.city()
+        active = 1
+        acccompany = ''
+        accname = ''
+        data.append([accid, password,email,taxid,groupnum,acccity,active,acccompany,accname])
+    w("Account.csv", data)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+######## CALL AREA #############
+
+# fill_customer()
+fill_account()
