b6051685799505dd461d9064774f15584aef9b0d
WZ <midas0231@gmail.com>
2023-12-22 14:56:23 -0500

    Encapsulate ML code
    Encapsulate ML code and removed extra model

--- backend/ml/ml.py
+++ backend/ml/ml.py
@@ -4,45 +4,50 @@ from sklearn.metrics import classification_report, confusion_matrix
 import pymysql
 import joblib
 
-# Connect to database
-db = pymysql.connect(
-    host='insurance-database.cdcuzzna0mo5.us-east-2.rds.amazonaws.com',
-    user='admin',
-    password='12345678',
-    port = 3306,
-    database="insdb"
-)
 
-# Query for parameters
-query = "SELECT Age, Gender, Income, Health_rating, Married, Purchased FROM Customer"
-with db.cursor() as cursor:
-    cursor.execute(query)
-    results = cursor.fetchall()
-age = [row[0] for row in results]
-gender = [row[1] for row in results]
-gender = [int(x) for x in gender]
-income = [row[2] for row in results]
-health_rating = [row[3] for row in results]
-married = [row[4] for row in results]
-purchased = [row[5] for row in results]
-data = list(zip(age, gender, income, health_rating, married))
+def train_knn():
+    # Connect to database
+    db = pymysql.connect(
+        host='insurance-database.cdcuzzna0mo5.us-east-2.rds.amazonaws.com',
+        user='admin',
+        password='12345678',
+        port=3306,
+        database="insdb"
+    )
 
-# 80% training set/ 20% testing set
-data_train, data_test, type_train, type_test = train_test_split(data, purchased, test_size=0.2)
-k = 10
+    # Query for parameters
+    query = "SELECT Age, Gender, Income, Health_rating, Married, Purchased FROM Customer"
+    with db.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+    age = [row[0] for row in results]
+    gender = [row[1] for row in results]
+    gender = [int(x) for x in gender]
+    income = [row[2] for row in results]
+    health_rating = [row[3] for row in results]
+    married = [row[4] for row in results]
+    purchased = [row[5] for row in results]
+    data = list(zip(age, gender, income, health_rating, married))
 
-# Create model
-knn = KNeighborsClassifier(n_neighbors=k)
-knn.fit(data_train, type_train)
+    # 80% training set/ 20% testing set
+    data_train, data_test, type_train, type_test = train_test_split(data, purchased, test_size=0.2)
+    k = 10
 
-# Save model
-joblib.dump(knn, 'knn.joblib')
+    # Create model
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(data_train, type_train)
+
+    # Save model
+    joblib.dump(knn, 'knn.joblib')
+    cursor.close()
+    db.close()
 
 # Test model
-knn = joblib.load('knn.joblib')
-prediction = knn.predict(data_test)
-print(confusion_matrix(type_test, prediction))
-print(classification_report(type_test, prediction))
+# knn = joblib.load('knn.joblib')
+# prediction = knn.predict(data_test)
+# print(confusion_matrix(type_test, prediction))
+# print(classification_report(type_test, prediction))
+
 
 def get_knn():
     knn1 = joblib.load('knn.joblib')
--- backend/ml/ml2.py
+++ backend/ml/ml2.py
@@ -3,49 +3,54 @@ from sklearn.model_selection import train_test_split
 import pymysql
 import joblib
 
-# Connect to database
-db = pymysql.connect(
-    host='insurance-database.cdcuzzna0mo5.us-east-2.rds.amazonaws.com',
-    user='admin',
-    password='12345678',
-    port = 3306,
-    database="insdb"
-)
 
-# Query for parameters
-query = """
-SELECT Age, Gender, Income, Health_rating, Married, C.Status FROM Contract C
-INNER JOIN Account_owner ON Account_owner.AccID = C.AccID
-INNER JOIN Customer ON Customer.Ssn = Account_owner.Ssn
-"""
-with db.cursor() as cursor:
-    cursor.execute(query)
-    results = cursor.fetchall()
-# process data
-age = [row[0] for row in results]
-gender = [row[1] for row in results]
-gender = [int(x) for x in gender]
-income = [row[2] for row in results]
-health_rating = [row[3] for row in results]
-married = [row[4] for row in results]
-status = [row[5] for row in results]
-data = list(zip(age, gender, income, health_rating, married))
+def train_reg():
+    # Connect to database
+    db = pymysql.connect(
+        host='insurance-database.cdcuzzna0mo5.us-east-2.rds.amazonaws.com',
+        user='admin',
+        password='12345678',
+        port=3306,
+        database="insdb"
+    )
 
-# 80% training set/ 20% testing set
-data_train, data_test, type_train, type_test = train_test_split(data, status, test_size=0.2)
+    # Query for parameters
+    query = """
+    SELECT Age, Gender, Income, Health_rating, Married, C.Status FROM Contract C
+    INNER JOIN Account_owner ON Account_owner.AccID = C.AccID
+    INNER JOIN Customer ON Customer.Ssn = Account_owner.Ssn
+    """
+    with db.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+    # process data
+    age = [row[0] for row in results]
+    gender = [row[1] for row in results]
+    gender = [int(x) for x in gender]
+    income = [row[2] for row in results]
+    health_rating = [row[3] for row in results]
+    married = [row[4] for row in results]
+    status = [row[5] for row in results]
+    data = list(zip(age, gender, income, health_rating, married))
 
-# Create model
-model = LogisticRegression()
-model.fit(data_train, type_train)
+    # 80% training set/ 20% testing set
+    data_train, data_test, type_train, type_test = train_test_split(data, status, test_size=0.2)
 
-# Save model
-joblib.dump(model, 'regression.joblib')
+    # Create model
+    model = LogisticRegression()
+    model.fit(data_train, type_train)
+
+    # Save model
+    joblib.dump(model, 'regression.joblib')
+    cursor.close()
+    db.close()
 
 # Test model
-model = joblib.load('regression.joblib')
-probabilities = model.predict_proba(data_test)[:, 1]
-rounded_probabilities = [round(prob, 3) for prob in probabilities]
-print(rounded_probabilities)
+# model = joblib.load('regression.joblib')
+# probabilities = model.predict_proba(data_test)[:, 1]
+# rounded_probabilities = [round(prob, 3) for prob in probabilities]
+# print(rounded_probabilities)
+
 
 def get_reg():
     reg = joblib.load('regression.joblib')
